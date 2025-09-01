ed0c9967e62d0913d8e32426a44820e83c5843e3
nils.nicolas <nils.nicolas@ird.fr>
2025-09-01 12:34:12 +1100

    SSL Certificat Serveur With Docker

--- certificat.local.sh
+++ certificat.local.sh
@@ -3,6 +3,19 @@ COMMAND="openssl req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:
 echo $COMMAND
 
 
+
+# Stopper tous les conteneurs en cours
+docker stop $(docker ps -aq)
+
+# Supprimer tous les conteneurs
+docker rm -f $(docker ps -aq)
+
+# (optionnel) Nettoyer images/volumes/réseaux inutilisés
+docker system prune -af            # sans les volumes
+# ou bien, pour tout raser y compris VOLUMES (perte de données) :
+docker system prune -af --volumes
+
+
 docker compose down -v || true
 # Construire l’image
 docker-compose build --no-cache
